6e6fd537ac2fd1df5df495e847b2f0952542d0f4
Svayam Mishra <svayamm@andrew.cmu.edu>
2017-06-21 11:06:00 -0400

    re-working img_sim for csv output

--- img_sim/__main__.py
+++ img_sim/__main__.py
@@ -38,44 +38,48 @@ class FindMatchingImages(object):
 
     def run(self):
         """ docstring for main run function """
-        self.labels_Path = Path('img_sim/test_output_img')
-        self.unclassified_Path = Path('img_sim/test_output_img')
+        # same_threshold = 20 # arbitrary value
+        self.labels_images_Path = Path('img_sim/classifier-labels/images')
+        self.unclassified_Path = Path('img_sim/unclassified-imgs-copy')
         self.output_Path = Path('img_sim/test_output_img')
 
         self.labels_list = [str(x) for x in self.labels_Path.iterdir() if x.is_dir()]
 
-        unclassified_images = create_image_list()
+        label_images_list = create_image_list(self.labels_images_Path)
+        # unclassified_images = create_image_list()
         find_matches_script.main(hs_dict, output_File)
 
-def create_new_empty_folders():
-    for label in self.labels_list:
-        # new_dir = Path resolve (output_path+label)
-        # if not new_dir.exists():
-        #     os.makedirs()
-        continue
+    def create_new_empty_folders(self):
+        """ docstring for main run function """
+        for label in self.labels_list:
+            # new_dir = Path resolve (output_path+label)
+            # if not new_dir.exists():
+            #     os.makedirs()
+            continue
 
-def create_image_list(img_Path):
+    def create_image_list(self, img_Path):
+        """ docstring for main run function """
+        if not img_Path.exists():
+            print('Image path does not exist!')
+        elif not img_Path.is_dir():
+            print('Image path is not a directory!')
+        else:
+            # obtains list of .png files in given directory
+            # -- can adjust to find multiple filetypes
+            images = img_Path.glob('**/*.png')
+            image_list = [image for image in images]
+        
+        return image_list
 
-    if not img_Path.exists():
-        print('Image path does not exist!')
-    elif not img_Path.is_dir():
-        print('Image path is not a directory!')
-    else:
-        # obtains list of .png files in given directory
-        # -- can adjust to find multiple filetypes
-        images = img_Path.glob('**/*.png')
-        image_list = [image for image in images]
-    
-    return image_list
-
-def classify_images(unclassified_list):
-    return_dict = {label:[] for label in self.labels_list}
-    for label in self.labels_list:
-        # label_path = self.classified_labels_path + label
-        # if not (self.label_path.exists() and label_path.is_dir() and os.listdir(label_path)):
-            # print 'self.invalid label_path' 
-        continue
-    return return_dict
+    def classify_images(self, self.unclassified_list):
+        """ docstring for main run function """
+        return_dict = {label:[] for label in self.labels_list}
+        for label in self.labels_list:
+            # label_path = self.classified_labels_path + label
+            # if not (self.label_path.exists() and label_path.is_dir() and os.listdir(label_path)):
+                # print 'self.invalid label_path' 
+            continue
+        return return_dict
     
 def sort_folders():
     # stuff
